23be3b27643a4b93bc94bb2b3adb8188fbe794d9
Lens <Lensdorsainvil711@outlook.com>
2023-02-19 15:35:01 -0500

    removed form variable

--- form.js
+++ form.js
@@ -1,11 +1,9 @@
 var dark_toggle = document.querySelector('.dark_toggle');
-var form = document.querySelector('form');
 var signUp = document.querySelector('.sign-in');
 var input = document.querySelectorAll('input');
 var button = document.querySelector('button');
 
 dark_toggle.addEventListener('click', function() {
-form.classList.toggle('dark');
 signUp.classList.toggle('dark');
 button.classList.toggle('dark_button')
 })
